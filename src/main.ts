fd420a6604649d074f982e4b9914c3ee9e9847ee
Nurhax <iqbalnur2009@gmail.com>
2025-10-03 18:36:09 +0700

    add new background and configuration

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    scale: {
+        mode: Phaser.Scale.FIT, // scale canvas to fit window
+        autoCenter: Phaser.Scale.CENTER_BOTH, // center horizontally & vertically
+        width: 1920, // your designed width
+        height: 1080 // your designed height
+    },
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { x: 200, y: 600 },
-            debug: false
-        }
+        default: "arcade",
+        arcade: { debug: false }
     },
     scene: [MainScene]
 };
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -3,8 +3,8 @@ import Player from '../player/Player';
 import Exhibit from '../objects/Exhibit';
 
 export default class MainScene extends Phaser.Scene {
-    private player!: Player;
-    private exhibit!: Exhibit;
+    // private player!: Player;
+    // private exhibit!: Exhibit;
 
     constructor() {
         super('MainScene');
@@ -12,25 +12,41 @@ export default class MainScene extends Phaser.Scene {
 
     preload(): void {
         // Load assets
-        this.load.image('background', '/images/background.png');
-        this.load.spritesheet('player', '/images/player.png', { frameWidth: 32, frameHeight: 48 });
-        this.load.image('exhibit', '/images/exhibit.png');
+        this.load.image('bgShadow', '/images/ShadowBackground.png');
+        this.load.image('platforms', '/images/Platform.png');
+        this.load.image('overlay', '/images/Overlayer.png');
+        // this.load.spritesheet('player', '/images/player.png', { frameWidth: 32, frameHeight: 48 });
+        // this.load.image('exhibit', '/images/exhibit.png');
     }
 
     create(): void {
-        this.add.image(1920, 1080, 'background');
+    const { width, height } = this.scale;
 
-        // Create player
-        this.player = new Player(this, 100, 450);
+    // Background
+    this.add.image(width / 2, height / 2, 'bgShadow')
+        .setOrigin(0.5)
+        .setScrollFactor(0);
 
-        // Create an interactable exhibit
-        this.exhibit = new Exhibit(this, 400, 500, 'exhibit', 'My Artwork Project');
+    // Platform example
+    this.add.image(width / 2, height - 100, 'platforms')
+        .setOrigin(0.5, 1);
 
-        // Camera follow
-        this.cameras.main.startFollow(this.player.sprite);
+    // Overlay (UI-ish)
+    this.add.image(width / 2, height / 2, 'overlay')
+        .setOrigin(0.5)
+        .setScrollFactor(0);
+
+        // // Create player
+        // this.player = new Player(this, 100, 450);
+
+        // // Create an interactable exhibit
+        // this.exhibit = new Exhibit(this, 400, 500, 'exhibit', 'My Artwork Project');
+
+        // // Camera follow
+        // this.cameras.main.startFollow(this.player.sprite);
     }
 
     update(time: number, delta: number): void {
-        this.player.update();
+        // this.player.update();
     }
 }
