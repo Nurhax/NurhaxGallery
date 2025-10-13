637f1d909fc58efa8718829204fbd4dc31308fe1
Nurhax <iqbalnur2009@gmail.com>
2025-10-13 19:56:36 +0700

    fix platform bugs

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -5,7 +5,7 @@ import Exhibit from '../objects/Exhibit';
 export default class MainScene extends Phaser.Scene {
     private player!: Player;
     private platforms!: Phaser.Physics.Arcade.StaticGroup;
-    // private exhibit!: Exhibit;
+
 
     constructor() {
         super('MainScene');
@@ -18,7 +18,7 @@ export default class MainScene extends Phaser.Scene {
         this.load.image('overlay', 'images/Overlayer.png');
         this.load.image('player_right', 'images/Haku_Idle_Right.png');
         this.load.image('player_left', 'images/Haku_Idle_Left.png');
-        // this.load.image('exhibit', '/images/exhibit.png');
+
     }
 
     // In MainScene.ts
@@ -34,33 +34,32 @@ export default class MainScene extends Phaser.Scene {
         // Platforms
         this.platforms = this.physics.add.staticGroup();
 
-        const floor = this.platforms.create(width / 2, height - 150, 'platforms')
+        const floor = this.platforms.create(width / 2, height , 'platforms')
             .setOrigin(0.5, 1)
-            .setScale(3, 1.5)
+            .setScale(1, 1)
             .refreshBody(); // This is crucial for static bodies
 
-        // Overlay
-        this.add.image(width / 2, height / 2, 'overlay')
-            .setOrigin(0.5)
-            .setScrollFactor(0);
-
+        
         // Get the absolute top coordinate of the floor's physics body
         const floorTopY = (floor.body as Phaser.Physics.Arcade.StaticBody).y;
-
+        
         // Create the player with its feet positioned exactly at the floor's top
         const playerTexture = this.textures.get('player_right');
         const playerFrame = playerTexture.getSourceImage();
         const playerHeight = playerFrame.height;
-
+        
         // Position so feet are at the floor's top
-        this.player = new Player(this, 350, floorTopY + playerHeight);
-
+        this.player = new Player(this, 350, floorTopY + 50); // 50 is an arbitrary offset to position above the floor
+        
         // Collisions
         this.physics.add.collider(this.player.sprite, this.platforms);
-
-        // Camera
-        this.cameras.main.startFollow(this.player.sprite);
+        
+        // Overlay
+        this.add.image(width / 2, height / 2, 'overlay')
+            .setOrigin(0.5)
+            .setScrollFactor(0);
     }
+
     update(time: number, delta: number): void {
         this.player.update();
     }
