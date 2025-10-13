6f76f3b59224302c67f2be052a6ee29d51c3d646
Nurhax <iqbalnur2009@gmail.com>
2025-10-13 10:29:12 +0700

    add new comments, assets. new bug: character and platform alignments

--- src/player/Player.ts
+++ src/player/Player.ts
@@ -3,30 +3,37 @@ import Phaser from 'phaser';
 export default class Player {
     public sprite: Phaser.Physics.Arcade.Sprite;
     private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    private facing: 'left' | 'right' = 'right';
 
     constructor(private scene: Phaser.Scene, x: number, y: number) {
-        this.sprite = scene.physics.add.sprite(x, y, 'player');
+        this.sprite = scene.physics.add.sprite(x, y, 'player_right');
+        this.sprite.setOrigin(0.5, 1); // anchor at feet
         this.sprite.setCollideWorldBounds(true);
+
+        // Resize player
+        this.sprite.setDisplaySize(350, 350);
 
         // Input
         this.cursors = scene.input!.keyboard!.createCursorKeys();
     }
 
     update(): void {
-        const speed = 160;
+        const speed = 200;
         const jumpSpeed = -330;
 
         if (this.cursors.left?.isDown) {
             this.sprite.setVelocityX(-speed);
-            this.sprite.flipX = true;
+            this.sprite.setTexture('player_left');
+            this.facing = 'left';
         } else if (this.cursors.right?.isDown) {
             this.sprite.setVelocityX(speed);
-            this.sprite.flipX = false;
+            this.sprite.setTexture('player_right');
+            this.facing = 'right';
         } else {
             this.sprite.setVelocityX(0);
         }
 
-        if (this.cursors.up?.isDown && this.sprite.body!.blocked.down) {
+        if (this.cursors.up?.isDown && (this.sprite.body as Phaser.Physics.Arcade.Body).blocked.down) {
             this.sprite.setVelocityY(jumpSpeed);
         }
     }
--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -3,7 +3,8 @@ import Player from '../player/Player';
 import Exhibit from '../objects/Exhibit';
 
 export default class MainScene extends Phaser.Scene {
-    // private player!: Player;
+    private player!: Player;
+    private platforms!: Phaser.Physics.Arcade.StaticGroup;
     // private exhibit!: Exhibit;
 
     constructor() {
@@ -12,41 +13,52 @@ export default class MainScene extends Phaser.Scene {
 
     preload(): void {
         // Load assets
-        this.load.image('bgShadow', '/images/ShadowBackground.png');
-        this.load.image('platforms', '/images/Platform.png');
-        this.load.image('overlay', '/images/Overlayer.png');
-        // this.load.spritesheet('player', '/images/player.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.image('bgShadow', 'images/ShadowBackground.png');
+        this.load.image('platforms', 'images/Platform.png');
+        this.load.image('overlay', 'images/Overlayer.png');
+        this.load.image('player_right', 'images/Haku_Idle_Right.png');
+        this.load.image('player_left', 'images/Haku_Idle_Left.png');
         // this.load.image('exhibit', '/images/exhibit.png');
     }
 
     create(): void {
-    const { width, height } = this.scale;
+        const { width, height } = this.scale;
 
-    // Background
-    this.add.image(width / 2, height / 2, 'bgShadow')
-        .setOrigin(0.5)
-        .setScrollFactor(0);
+        // Background
+        this.add.image(width / 2, height / 2, 'bgShadow')
+            .setOrigin(0.5)
+            .setScrollFactor(0);
 
-    // Platform example
-    this.add.image(width / 2, height, 'platforms')
-        .setOrigin(0.5, 1);
+        // Platforms
+        this.platforms = this.physics.add.staticGroup();
 
-    // Overlay (UI-ish)
-    this.add.image(width / 2, height / 2, 'overlay')
-        .setOrigin(0.5)
-        .setScrollFactor(0);
+        // Platform example
+        const floor = this.platforms.create(width / 2, height + 150, 'platforms')
+            .setOrigin(0.5,1)
+            .setScale(3, 1.5) // stretch to fit width
+            .refreshBody();
 
-        // // Create player
-        // this.player = new Player(this, 100, 450);
+        // Overlay (UI-ish)
+        this.add.image(width / 2, height / 2, 'overlay')
+            .setOrigin(0.5)
+            .setScrollFactor(0);
+
+        // Create player
+        const floorTop = floor.getBounds().top;
+        this.player = new Player(this, 1000, 1000);
+
+        // Collisions
+        this.physics.add.collider(this.player.sprite, this.platforms);
 
         // // Create an interactable exhibit
         // this.exhibit = new Exhibit(this, 400, 500, 'exhibit', 'My Artwork Project');
 
         // // Camera follow
-        // this.cameras.main.startFollow(this.player.sprite);
+        this.cameras.main.startFollow(this.player.sprite);
     }
 
     update(time: number, delta: number): void {
-        // this.player.update();
+        this.player.update();
+
     }
 }
